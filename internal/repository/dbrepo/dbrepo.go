01b93e5d3e2ac85a414482c7ff306776319770dd
Rahul Dhiman <rsdm34@gmail.com>
2023-11-11 12:30:18 -0800

    postgres db setup

--- internal/repository/dbrepo/dbrepo.go
+++ internal/repository/dbrepo/dbrepo.go
@@ -1,1 +1,16 @@
 package dbrepo
+
+import (
+	"InrixBackend/internal/repository"
+	"database/sql"
+)
+
+type postgresDBRepo struct {
+	DB *sql.DB
+}
+
+func NewPostgresRepo(conn *sql.DB) repository.DatabaseRepo {
+	return &postgresDBRepo{
+		DB: conn,
+	}
+}
